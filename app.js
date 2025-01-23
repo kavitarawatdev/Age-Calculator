63fab2f46a07d070904f29a84509e9482dd263e0
kavitarawatdev <ikavitarawat227@gmail.com>
2025-01-23 18:28:52 +0530

    clear interval function

--- app.js
+++ app.js
@@ -2,68 +2,97 @@ let calculate = document.querySelector(".solve");
 let result = document.querySelector (".result");
 let reset = document.querySelector(".reset");
 let userInput = document.getElementById("DOB");
-let years = document.querySelector(".years input");
-let months = document.querySelector(".months input");
-let days = document.querySelector(".days input");
-let hours = document.querySelector(".hours input");
-let minutes = document.querySelector(".minutes input");
-let seconds = document.querySelector(".seconds input");
+let age = document.querySelectorAll(".age");
+let timer;
 
 
+// function for reset
 const resetButton = () => {
+    clearInterval(timer)
     result.style.display = "none";
     userInput.value = "";
     reset.style.display = "none";
 }
+
+// function for showing age
+const showAge =(Y, M, D, H, Min, S )=>{
+    age.forEach((ele,i)=>{
+        if (i==0) ele.value=Y;
+        if (i==1) ele.value=M;
+        if (i==2) ele.value=D;
+        if (i==3) ele.value=H;
+        if (i==4) ele.value=Min;
+        if (i==5) ele.value=S;
+    })
+}
+
+// // function for calculating year
+const calYear = (Yi, Yf, Mi, Mf, Di, Df)=>{
+    if( Mi<Mf || (Mi==Mf && Di<=Df) ){
+        return (Math.abs(Yi-Yf))
+    }else if ( Mi>Mf || (Mi==Mf && Di>Df) ){
+        return (Math.abs(Yi-Yf)-1)
+    }
+}
+
+// // function for calculating month
+const calMonth = (Mi, Mf, Di, Df)=>{
+    if( (Mi<Mf) || (Mi==Mf && Di<=Df) ){
+       return Math.abs(Mf-Mi)
+    }else if((Mi>Mf) || (Mi==Mf && Di>Df)){
+        return (12-Mi)+Mf
+    }
+}
+
+// // function for calculating day
+const calDay = (Di, Df)=>{
+   return Df>=Di? Math.abs(Df-Di):(31-Di)+Df;
+}
+
 // ====================================
 //                calculating age
 // ====================================
 const ageCount = () => {
-    let now = new Date();                           
-    let currentY = now.getFullYear();                
-    let currentM = now.getMonth(); 
-    let currentD = now.getDay(); 
-    let currentH = now.getHours(); 
-    let currentMin = now.getMinutes(); 
-    let currentS = now.getSeconds();                 
-      
-    let inputDOB = userInput.value;   
-    let dob = new Date(inputDOB);                            
-    let dobY = dob.getFullYear();                         
-    let dobM = dob.getMonth(); 
-    let dobD = dob.getDay();  
-    let dobH = dob.getHours();
-    let dobMin = dob.getMinutes();
-    let dobS = dob.getSeconds();                          
-      
-    let ageY = currentY - dobY;
-    let ageM = Math.abs(currentM - dobM); 
-    let ageD = Math.abs(currentD - dobD);  
-    let ageH = Math.abs(currentH - dobH);
-    let ageMin = Math.abs(currentMin - dobMin);
-    let ageS = Math.abs(currentS - dobS);     
+    clearInterval(timer);
+    let year, month, day, hour, mins, secs;  
+    // getting curr date data
+    const currDate = new Date().toLocaleDateString('en-GB').split("/");
+    const currDay = Number(currDate[0]);
+    const currMonth = Number(currDate[1]);
+    const currYear = Number(currDate[2]);
+
+    // getting Curr time
+    const currTime = new Date().toLocaleTimeString().replace(/[^0-9:]/g, '').split(":");
+    hour= currTime[0]
+    mins = currTime[1]
+    secs = currTime[2]
+
+    // getting birthdate date
+    const birthDate = new Date(userInput.value).toLocaleDateString('en-GB').split("/");
+    let birthDay = Number(birthDate[0]);
+    let birthMonth = Number(birthDate[1]);
+    let birthYear = Number(birthDate[2]);
+
+    year = calYear(birthYear, currYear, birthMonth, currMonth, birthDay, currDay)
+    month = calMonth(birthMonth, currMonth, birthDay, currDay)
+    day = calDay( birthDay, currDay)
 
     result.style.display = "flex";
     reset.style.display = "block";
+    showAge(year, month, day, hour, mins, secs)
 
-    years.value = ageY;
-    months.value = ageM;
-    days.value = ageD;
-    hours.value = ageH;
-    minutes.value = ageMin;
-    seconds.value = ageS; 
-
-    setInterval(() => {
-        seconds.value++;
-        if(seconds.value==="60"){
-            seconds.value="0";
-            minutes.value++;
-            if(minutes.value==="60"){
-                minutes.value="0"
-                hours.value++;
+    timer = setInterval(() => {
+        age[5].value++;
+        if(age[5].value=="60"){
+            age[5].value="0";
+            age[4].value++; 
+            if(age[4].value=="60"){
+                age[4].value="0";
+                age[3].value++;
             }
         }
     }, 1000);
+
 }
 
 calculate.addEventListener("click", ageCount);
