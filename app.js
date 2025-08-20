2ed79f9333a08969575df090468c5ef8bcfb744c
kavitarawatdev <ikavitarawat227@gmail.com>
2025-08-20 12:42:10 +0530

    add theme selector for better ui

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ let result = document.querySelector (".result");
 let reset = document.querySelector(".reset");
 let userInput = document.getElementById("DOB");
 let age = document.querySelectorAll(".age");
+let userTheme=document.querySelector("#themeSelect");
 let timer;
 
+// function to set theme
+const setTheme = () => {
+    document.body.className = userTheme.value;
+}
+
+userTheme.addEventListener("change",()=>{
+    setTheme()
+
+});
 
 // function for reset
 const resetButton = () => {
@@ -27,26 +37,34 @@ const showAge =(Y, M, D, H, Min, S )=>{
 }
 
 // // function for calculating year
-const calYear = (Yi, Yf, Mi, Mf, Di, Df)=>{
-    if( Mi<Mf || (Mi==Mf && Di<=Df) ){
-        return (Math.abs(Yi-Yf))
-    }else if ( Mi>Mf || (Mi==Mf && Di>Df) ){
-        return (Math.abs(Yi-Yf)-1)
+const calYear = (birthYear, currYear, birthMonth, currMonth, birthDay, currDay)=>{
+    if( birthMonth<currMonth || (birthMonth==currMonth && birthDay<=currDay) ){
+        return (Math.abs(birthYear-currYear))
+    }else if ( birthMonth > currMonth || (birthMonth==currMonth && birthDay>currDay) ){
+        return (Math.abs(birthYear-currYear)-1)
     }
 }
 
 // // function for calculating month
-const calMonth = (Mi, Mf, Di, Df)=>{
-    if( (Mi<Mf) || (Mi==Mf && Di<=Df) ){
-       return Math.abs(Mf-Mi)
-    }else if((Mi>Mf) || (Mi==Mf && Di>Df)){
-        return (12-Mi)+Mf
+const calMonth = (birthMonth, currMonth, birthDay, currDay)=>{
+    if( (birthMonth < currMonth) || (birthMonth==currMonth && birthDay<=currDay) ){
+       return Math.abs(currMonth-birthMonth)
+    }else if((birthMonth>currMonth) || (birthMonth==currMonth && birthDay>currDay)){
+        return (12-birthMonth)+currMonth
     }
 }
 
 // // function for calculating day
-const calDay = (Di, Df)=>{
-   return Df>=Di? Math.abs(Df-Di):(31-Di)+Df;
+const calDay = (birthYear, birthMonth, birthDay, currYear, currMonth, currDay) => {
+    if (currDay >= birthDay) {
+        return currDay - birthDay;
+    } else {
+        // Get days in previous month
+        const prevMonth = currMonth === 1 ? 12 : currMonth - 1;
+        const prevYear = currMonth === 1 ? currYear - 1 : currYear;
+        const daysInPrevMonth = new Date(prevYear, prevMonth, 0).getDate();
+        return (daysInPrevMonth - birthDay) + currDay;
+    }
 }
 
 // ====================================
@@ -54,43 +72,54 @@ const calDay = (Di, Df)=>{
 // ====================================
 const ageCount = () => {
     clearInterval(timer);
+
+    //if input is not valid
+    if (!userInput.value) {
+        alert('Please select your date of birth');
+        return;
+    }
+    
     let year, month, day, hour, mins, secs;  
-    // getting curr date data
-    const currDate = new Date().toLocaleDateString('en-GB').split("/");
-    const currDay = Number(currDate[0]);
-    const currMonth = Number(currDate[1]);
-    const currYear = Number(currDate[2]);
-
+    // getting curr date date
+    const now = new Date();
+    const currDay = now.getDate();
+    const currMonth = now.getMonth() + 1;  // +1 because getMonth() is 0-based
+    const currYear = now.getFullYear();
     // getting Curr time
-    const currTime = new Date().toLocaleTimeString().replace(/[^0-9:]/g, '').split(":");
-    hour= currTime[0]
-    mins = currTime[1]
-    secs = currTime[2]
+    hour = now.getHours();
+    mins = now.getMinutes();
+    secs = now.getSeconds();
 
-    // getting birthdate date
-    const birthDate = new Date(userInput.value).toLocaleDateString('en-GB').split("/");
-    let birthDay = Number(birthDate[0]);
-    let birthMonth = Number(birthDate[1]);
-    let birthYear = Number(birthDate[2]);
+    //getting brith date
+    const birthDate = new Date(userInput.value);
 
+    // Check if the input date is valid
+    if (birthDate > now) {
+        alert('Date of birth cannot be in the future');
+        return;
+    }
+
+    let birthDay = birthDate.getDate();
+    let birthMonth = birthDate.getMonth() + 1;  // +1 because getMonth() returns 0-11
+    let birthYear = birthDate.getFullYear();
+    
     year = calYear(birthYear, currYear, birthMonth, currMonth, birthDay, currDay)
     month = calMonth(birthMonth, currMonth, birthDay, currDay)
-    day = calDay( birthDay, currDay)
+    day = calDay( birthYear, birthMonth, birthDay, currYear, currMonth, currDay)
 
     result.style.display = "flex";
     reset.style.display = "block";
     showAge(year, month, day, hour, mins, secs)
 
     timer = setInterval(() => {
-        age[5].value++;
-        if(age[5].value=="60"){
-            age[5].value="0";
-            age[4].value++; 
-            if(age[4].value=="60"){
-                age[4].value="0";
-                age[3].value++;
-            }
-        }
+        const now = new Date();
+        const currentSecs = now.getSeconds();
+        const currentMins = now.getMinutes();
+        const currentHours = now.getHours();
+    
+        age[5].value = currentSecs;
+        age[4].value = currentMins;
+        age[3].value = currentHours;
     }, 1000);
 
 }
